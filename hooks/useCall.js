f10edf06e4ea2b4a181d18bbe86f928af23aa4ec
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-26 12:38:21 +0200

    added fome turn servers

--- hooks/useCall.js
+++ hooks/useCall.js
@@ -79,6 +79,31 @@ export default function useCall(uuid, socket)
                     username: "openrelayproject",
                     credential: "openrelayproject",
                 },
+                {
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=udp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=tcp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                },
+                {
+                    url: 'turn:turn.bistri.com:80',
+                    credential: 'homeo',
+                    username: 'homeo'
+                 },
+                 {
+                    url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+                    credential: 'webrtc',
+                    username: 'webrtc'
+                }
             ]
         });
 
