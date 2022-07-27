e42b64ad06e6e7f77ff9382f29b5ca5d799bd8a3
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-27 14:51:37 +0200

    overflow y auto on topics and updated todos

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Debate Bro
 
 Easily find someone to debate online
+
+## TODO
+* change drag library to react-kit since current library is ugly
--- components/DroppableTopicList.js
+++ components/DroppableTopicList.js
@@ -9,7 +9,7 @@ export default function DroppableTopicList({ title, droppableId, topicsList }) {
             <Droppable ignoreContainerClipping={true} direction='grid' droppableId={droppableId}>
                 {(provided) => (
                     <div {...provided.droppableProps} ref={provided.innerRef} 
-                         className='min-h-[12rem] overflow-x-clip overflow-y-scroll max-h-[12rem]'>
+                         className='min-h-[12rem] overflow-x-clip overflow-y-auto max-h-[12rem]'>
                         <div
                             className='flex flex-wrap w-80 align-center justify-center'>
                             {topicsList.map((topic, index) =>
