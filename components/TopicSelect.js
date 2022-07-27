c63cc33cfbec8e2163f7dfa88732cd39e928bb2d
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-27 12:59:42 +0200

    topic selection

--- components/TopicSelect.js
+++ components/TopicSelect.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import TopicElem from './TopicElem'
+import { v4 as uuidv4 } from 'uuid'
 
-export default function TopicSelect({ selectedTopics, setSelectedTopics }) {
+const allTopics = ['Veganism', 'Abortion', 'Communism', 'weed', 'penis']
+
+export default function TopicSelect({  }) {
+  const [availableTopics, setAvailableTopics] = useState(allTopics)
+  const [selectedTopics, setSelectedTopics] = useState([])
+  
+  function addTopic(topic)
+  {
+    setAvailableTopics(old => old.filter(elem => elem !== topic))
+    setSelectedTopics(old => [...old, topic])
+  }
+
+  function removeTopic(topic)
+  {
+    setSelectedTopics(old => old.filter(elem => elem !== topic))
+    setAvailableTopics(old => [...old, topic])
+  }
+
   return (
-    <div>TopicSelect</div>
+    <>
+    <div className='flex flex-col my-4 text-center'>
+      <p className='underline text-2xl'>Topics</p>
+      <div className='flex flex-wrap flex-row justify-center'>
+        {availableTopics.map(topic => <TopicElem onClick={addTopic} key={topic} topic={topic} />)}
+      </div>
+    <div className='flex flex-col my-4 text-center'>
+    <p className='underline text-2xl'>Selected Topics</p>
+    <div className='flex flex-wrap flex-row justify-center'>
+      {selectedTopics.map(topic => <TopicElem onClick={removeTopic} key={topic} topic={topic} />)}
+    </div>
+    </div>
+    </div>
+    </>
+
+    
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState, createContext } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import io from 'socket.io-client'
 import useCall from '../hooks/useCall';
+import TopicSelect from '../components/TopicSelect';
 
 export const SocketContext = createContext();
 const uuid = uuidv4()
@@ -44,33 +45,32 @@ export default function SocketTest() {
     return (
         <>
             <div className='h-screen w-full text-simvoni flex text-center flex-col text-white bg-spacecadet '>
-            <h1 className='text-8xl mt-20'><span className='text-frenchskyblue'>debate</span> app</h1>
-            <div className='flex flex-col lg:flex-row mx-20 my-10 gap-20 justify-center align-center self-center'>
-                
-                <video muted 
-                       autoPlay="true" 
-                       ref={ourStreamRef} 
-                       className={`block self-center ${callConnected
-                       ? `w-[32rem] h-[24rem] 
-                       2xl:w-[44rem] 2xl:h-[33rem]
-                       3xl:w-[56rem] 3xl:h-[42rem]`
-                       : 'w-[60rem] h-[45rem]'}`}>
-                </video>
+            <h1 className='text-8xl mt-10'><span className='text-frenchskyblue'>debate</span> app</h1>
+            <div className='flex flex-col lg:flex-row mx-20 my-10 justify-evenly align-center self-center'>
+                    <video muted 
+                        autoPlay="true" 
+                        ref={ourStreamRef} 
+                        className={`border-[0.5rem] rounded-2xl object-cover border-frenchskyblue 
+                        block self-center ${callConnected
+                        ? `w-[32rem] h-[24rem] 
+                        2xl:w-[44rem] 2xl:h-[33rem]
+                        3xl:w-[56rem] 3xl:h-[42rem]`
+                        : 'w-[60rem] h-[45rem]'}`}>
+                    </video>
 
                 <video autoPlay="true" 
                        ref={theirStreamRef} 
-                       className={`block self-center ${callConnected ? '' : 'hidden'} 
+                       className={`border-[0.5rem] rounded-2xl object-cover border-frenchskyblue block self-center ${callConnected ? '' : 'hidden'} 
                         w-[32rem] h-[24rem] 
                        2xl:w-[44rem] 2xl:h-[33rem]
                        3xl:w-[56rem] 3xl:h-[42rem]`}>
                 </video>
             </div>
             <p className='text-3xl font-semibold '><span className='underline'>current topic:</span> <span className=' text-yellow-500'>Veganism</span> </p>
-            <div className='mt-10 text-center flex flex-col self-center'>
-                    <p>Socket.io ({uuid})</p>
-                    <div></div>
+            <div className='text-center flex-grow-0 flex-shrink-0 w-96 flex flex-col self-center'>
                     
-                    <div onClick={findOpponent} className='p-2 bg-frenchskyblue block rounded-lg hover:cursor-pointer'>
+                    <TopicSelect />
+                    <div onClick={findOpponent} className='p-2 text-3xl bg-frenchskyblue block rounded-lg hover:cursor-pointer'>
                         Find Opponent
                     </div>
                     <div>
