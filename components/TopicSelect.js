ec8ec2b55ffe7eb21486633383d56cd447b98813
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-27 19:20:43 +0200

    topic css optimizations mobile

--- components/TopicSelect.js
+++ components/TopicSelect.js
@@ -55,14 +55,14 @@ export default function TopicSelect({ }) {
     <>
       <DragDropContext onDragStart={() => setIsDragging(true)} onDragEnd={handleDragEnd}>
         <div className='flex flex-row justify-between gap-4 md:gap-6 lg:gap-8 xl:gap-10 h-[18rem] my-4 text-center'>
-          <div className='flex flex-col mt-2 justify-end '>
+          <div className='flex flex-col mt-4 '>
             <DroppableTopicList isDragging={isDragging} title={'I want to debate in favor of'} droppableId={'defendTopics'} topicsList={defendTopics} />
           </div>
-          <div className='flex flex-col mt-2 align-bottom'>
+          <div className='flex flex-col mt-4 '>
             <DroppableTopicList isDragging={isDragging} title={'Available Topics'} droppableId={'availableTopics'} topicsList={availableTopics} />
           </div>
 
-          <div className='flex flex-col mt-2 align-bottom'>
+          <div className='flex flex-col mt-4 '>
             <DroppableTopicList isDragging={isDragging} title={'I want to debate against'} droppableId={'attackTopics'} topicsList={attackTopics} />
           </div>
         </div>
