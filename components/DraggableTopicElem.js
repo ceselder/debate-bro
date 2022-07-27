ab66769c40e43cb4c7383093512fd647539b70cf
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-27 18:10:24 +0200

    topic animation compromise (good enough for now)

--- components/DraggableTopicElem.js
+++ components/DraggableTopicElem.js
@@ -3,13 +3,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Draggable } from 'react-beautiful-dnd'
 
+function getStyle(style, snapshot) {
+  if (!snapshot.isDropAnimating) {
+    return style;
+  }
+  return {
+    ...style,
+    // cannot be 0, but make it super tiny
+    transitionDuration: `0.001s`,
+  };
+}
+
+
 export default function DraggableTopicElem({ topic, index }) {
   return (
     (<Draggable key={topic} draggableId={topic} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div {...provided.draggableProps}
              {...provided.dragHandleProps} 
              ref={provided.innerRef}
+             style={getStyle(provided.draggableProps.style, snapshot)}
           className=' hover:cursor-pointer h-min p-2 bg-frenchskyblue m-1 
                 rounded-lg'
         >
