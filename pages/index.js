a0da68b9c564780597470a42beff0ee01382c6eb
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-29 16:38:19 +0200

    removed unnecessary getstaticprops call

--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,12 @@ const allTopics = ['Veganism', 'Abortion', 'Communism',
 'Social Democracy', 'Socialism', 'Racism'
 , 'Immigration', 'CRT', 'BLM']
 
-export async function getStaticProps() {
-
-    return { props: { turnpass: process.env.TURN_PASS } }
-}
-
 export const topicContext = createContext() 
 
-export default function App({ turnpass }) {
+export default function App() {
     const [socket, setSocket] = useState(null)
     const [events, setEvents] = useState([])
-    const [callConnected, ourStreamRef, theirStreamRef] = useCall(uuid, socket, turnpass)
+    const [callConnected, ourStreamRef, theirStreamRef] = useCall(uuid, socket)
 
     const [availableTopics, setAvailableTopics] = useState(allTopics)
     const [defendTopics, setDefendTopics] = useState([])
