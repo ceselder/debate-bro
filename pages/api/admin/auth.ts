0140ece493ce6d5ab174ce2860b21a1adec13df6
Ryan <iperish.x@gmail.com>
2022-08-02 21:01:07 +0200

    remove logging that isnt needed

--- pages/api/admin/auth.ts
+++ pages/api/admin/auth.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export const auth = (req: NextApiRequest, res: NextApiResponse) => {
-    console.log(req.body.data);
     if (req.body.data === process.env.ADMIN_PAGE_PASSWORD) {
         return res.status(200).send("ok");
     } else {
